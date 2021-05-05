a15f149cd838f385b707b3f72844dfb5d55252c3
vinay2201 <vinay220199@gmail.com>
2021-05-05 13:42:23 +0530

    This code with 3 lines in while loop

---
+++ EmployeeServlet.java
@@ -0,0 +1,59 @@
+package com.employee;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class EmployeeServlet extends GenericServlet  
+{private static final long serialVersionUID = 1L;
+    protected void doPost(HttpServletRequest req,HttpServletResponse res)throws ServletException,IOException
+    {
+        PrintWriter pw=res.getWriter();
+        res.setContentType("text/html");        
+        String tb=req.getParameter("table");    
+
+        try
+        {
+             Class.forName("oracle.jdbc.driver.OracleDriver");
+             Connection con=DriverManager.getConnection("jdbc:oracle:thin:"
+ 					+ "@localhost:1521:ORCL", "system", "vinay");
+             Statement st=con.createStatement();
+             System.out.println("connection established successfully...!!");     
+
+             ResultSet rs=st.executeQuery("Select * from "+tb);
+
+             pw.println("<table border=1>");
+                 while(rs.next())
+                 {
+                     pw.println("<tr><td>"+rs.getInt(1)+"</td><td>"+rs.getString(2)+"</td>"+
+                                      "<td>"+rs.getString(3)+"</td>"+"<td>"+rs.getInt(4)+"</td>"+"<td>"+
+                                      rs.getDate(5)+"</td>"+"<td>"+rs.getInt(6)+"</td>"
+                                      +"<td>"+rs.getInt(7)+"</td>"+"<td>"+rs.getInt(8)+"</td></tr>");
+                 }
+             pw.println("</table>");
+             pw.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+	@Override
+	public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+}
